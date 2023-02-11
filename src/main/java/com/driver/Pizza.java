1c3d8a316b7e2b83f4b6a628bab94cc5cedb47c6
wambexpeter <wambexpeter@gmail.com>
2023-02-11 16:20:51 +0300

    pizzanew

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -69,7 +69,7 @@ public class Pizza {
             if (isTakeAway) {
                 this.bill += "Paperbag Added: 20" + "\n";
             }
-            this.bill += "Total Price: " + this.price;
+            this.bill += "Total Price: " + this.price +"\n";
             this.isBillCreated = true;
 
 
